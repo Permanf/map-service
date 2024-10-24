751e9986863fe840dfecfc39aa6989fe92a13806
Permanf <tirkishmyradovperman@gmail.com>
2024-10-24 11:17:51 +0500

    added land use planning layers api

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ import fetch from 'node-fetch';
 import express from 'express';
 
 const app = express();
+app.get('/api/landUsePlanning', async (req, res) => {
+    try {
+      const url = 'https://gisservices.chathamcountync.gov/opendataagol/rest/services/LandUsePlanning/Chatham_PittsboroZoning/MapServer/layers?f=json';
+      
+      // landUsePlanning
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data,"---1")
+  
+      res.json(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).json({ message: 'Error fetching data' });
+    }
+});
 
 app.get('/api/map-service', async (req, res) => {
   try {
-    const urlLine = 'https://gis11.services.ncdot.gov/arcgis/rest/services/NCDOT_STIP/MapServer/1/query?f=json&where=(1=1) AND (1=1)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=OBJECTID ASC&outSR=102100&resultOffset=0&resultRecordCount=50';
+    const urlLine = 'https://gis11.services.ncdot.gov/arcgis/rest/services/NCDOT_STIP/MapServer/1/query?f=json&where=(1=1) AND (1=1)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=OBJECTID ASC&outSR=102100';
     const urlDot = 'https://gis11.services.ncdot.gov/arcgis/rest/services/NCDOT_STIP/MapServer/0/query?f=json&where=(1=1) AND (1=1)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=OBJECTID ASC&outSR=102100'
     
     // Line
@@ -28,6 +43,8 @@ app.get('/api/map-service', async (req, res) => {
         attributesArray.push(feature.attributes);
       });
     }
+
+    console.log(attributesArray,"---2")
 
     res.json(attributesArray);
   } catch (error) {
